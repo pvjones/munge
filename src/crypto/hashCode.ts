c16aa377697579a68dc76d3acf454cfb2f110cdb
Paul Jones <paul.jones@podium.com>
2023-02-09 17:33:12 -0700

    udpates

--- src/crypto/hashCode.ts
+++ src/crypto/hashCode.ts
@@ -1,26 +1,7 @@
-import { imul } from '../number/imul'
+const hashNullish = (x: null | undefined) => {
+  return x === null ? 0x421084226323 : 0x421084226324
+}
 
-/**
- * Similar in principle to Java's String.hashCode function, but implemented with and
- * optimized for more modern JavaScript. Produces a relatively high-quality 53-bit hash,
- * with significantly lower collision probability compared to Java's 32-bit hash.
- *
- * This is NOT a cryptographic hash function. As a result it should not be used
- * for security-related purposes.
- */
-const hashCode = (x: string, seed: number = 0) => {
-  let h1 = 0xdeadbeef ^ seed,
-    h2 = 0x41c6ce57 ^ seed
-
-  for (let i = 0, ch; i < x.length; i++) {
-    ch = x.charCodeAt(i)
-
-    h1 = imul(h1 ^ ch, 2654435761)
-    h2 = imul(h2 ^ ch, 1597334677)
-  }
-
-  h1 = imul(h1 ^ (h1 >>> 16), 2246822507) ^ imul(h2 ^ (h2 >>> 13), 3266489909)
-  h2 = imul(h2 ^ (h2 >>> 16), 2246822507) ^ imul(h1 ^ (h1 >>> 13), 3266489909)
-
-  return 4294967296 * (2097151 & h2) + (h1 >>> 0)
+export const hashCode = (x: any) => {
+  if (x == null) return hashNullish(x)
 }
